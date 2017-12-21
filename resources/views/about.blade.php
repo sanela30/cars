f6bd8c024dd970fa73089a1f3e503b46903b8411
Academy <sanela26@gmail.com>
2017-12-21 19:04:36 +0100

    prvi komit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+      <title>AboutPage</title>
+    </head>
+    <body>
+        <p>about us</p>
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+      <title>welcomePage</title>
+    </head>
+    <body>
+        <p>Welcome {{ $name }}</p>
+        <p>I'm {{ $age }} years old</p>
+    </body>
+</html>
